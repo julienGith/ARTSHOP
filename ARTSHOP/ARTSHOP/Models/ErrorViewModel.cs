a2232b5debc2d327011a17d59c4dded3294a8ffa
julienGith <53513531+julienGith@users.noreply.github.com>
2021-03-14 12:00:48 +0100

    1stvsmvc

---
+++ ARTSHOP/ARTSHOP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ARTSHOP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
