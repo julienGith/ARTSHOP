86e07bd4a6f6cc63875638ba27ff947a3623cd86
julienGith <53513531+julienGith@users.noreply.github.com>
2021-03-16 17:03:01 +0100

    Update Iboutique.cs

--- ShopApp/DAL/Interfaces/Iboutique.cs
+++ ShopApp/DAL/Interfaces/Iboutique.cs
@@ -9,14 +9,19 @@ namespace DAL.Interfaces
 {
     public interface Iboutique
     {
+        //CRUD Boutique
+        //ADD Boutique
         Task<Boutique> AddBoutique(int partenaireid, int politiqueid, string descriptionC,
             string descriptionL, string raisonsociale, string siret, string siren, string tel,
             string codenaf, string codebanque, string codeguichet, string numcompte, string clerib,
             string domiciliation, string iban, string bic, string titulaire, string mail,
             string message, int ca, int nbsalarie, string siteweb, string statutjuridique, string btqseo);
         
-        Task<List<Boutique>> GetPartenaireBoutiques(string partenaireID);
 
+        //Recherche Boutique
+        //Mes Boutiques
+        Task<List<Boutique>> GetPartenaireBoutiques(string partenaireID);
+        //GET All Boutiques
         Task<List<Boutique>> GetAllBoutiques();
 
     }
