5ce3d4628d22ce8eb2f21242dc7a0f085422e829
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-01 12:16:07 +0200

    categorie

---
+++ E-Shop/E-Shop/Data/Functions/CategrieFunctions.cs
@@ -0,0 +1,63 @@
+﻿using E_Shop.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Shop.Data.Functions
+{
+    public class CategrieFunctions
+    {
+        //CRUD
+        //Créer une catégorie
+        public async Task<Categorie> AddCategorie(string catNom, int CatParentID)
+        {
+            if (CatParentID==0)
+            {
+                Categorie categorie = new Categorie
+                {
+                    Categorienom = catNom
+                };
+                using (var context = new ApplicationDbContext(ApplicationDbContext.ops.dbOptions))
+                {
+                    context.Categories.Add(categorie);
+                    context.SaveChanges();
+                }
+                return categorie;
+
+            }
+            else
+            {
+                Categorie categorie = new Categorie
+                {
+                    Categorienom = catNom
+                };
+                Categorie categorieParent = new Categorie();
+                Catnav catnav = new Catnav();
+                using (var context = new ApplicationDbContext(ApplicationDbContext.ops.dbOptions))
+                {
+                    context.Categories.Add(categorie);
+                    categorieParent = await context.Categories.FirstOrDefaultAsync(c => c.Categorieid == CatParentID);
+                    catnav = await context.Catnavs.FirstAsync(c => c.CatCategorieid == categorieParent.Categorieid);
+                    if (catnav==null)
+                    {
+                        catnav.CatCategorieid = categorieParent.Categorieid;
+                        catnav.Categorieid = categorie.Categorieid;
+                        context.Catnavs.Add(catnav);
+                        context.SaveChanges();
+
+                    }
+                    else
+                    {
+                        catnav.Categorie.Categorieid = categorie.Categorieid;
+                        context.Catnavs.Update(catnav);
+                        context.SaveChanges();
+                    }
+                }
+                return categorie;
+
+            }
+        }
+    }
+}
---
+++ E-Shop/E-Shop/Pages/Categorie/CreateCategorie.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Shop.Pages.Categorie
+{
+    public class CreateCategorieModel : PageModel
+    {
+        public Input input { get; set; }
+        public class Input
+        {
+            public string CatNom { get; set; }
+            ICollection<string>Categories { get; set; }
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
