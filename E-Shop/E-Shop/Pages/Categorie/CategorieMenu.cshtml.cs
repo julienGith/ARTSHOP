7f245e5080c5bfc872e69610bb468dadd7297c71
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-03 03:10:26 +0200

    up

--- E-Shop/E-Shop/Pages/Categorie/CategorieMenu.cshtml.cs
+++ E-Shop/E-Shop/Pages/Categorie/CategorieMenu.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using E_Shop.Logic.CategorieLogic;
 using Microsoft.AspNetCore.Mvc;
@@ -10,18 +11,28 @@ namespace E_Shop.Pages.Categorie
 {
     public class CategorieMenuModel : PageModel
     {
+        StringBuilder stringBuilder = new StringBuilder();
+
         private CategorieLogic categorieLogic = new CategorieLogic();
+        public List<Entities.Categorie> CatParentsAlim = new List<Entities.Categorie>();
+        public List<Entities.Categorie> CatEnfants1 = new List<Entities.Categorie>();
+        public List<Entities.Categorie> CatEnfants2 = new List<Entities.Categorie>();
 
-        public class catmenu
-        {
-            public string CategorieNom { get; set; }
-            public int CategorieId { get; set; }
 
-        }
 
         public async Task OnGet()
         {
-           await categorieLogic.();
+            CatParentsAlim = await categorieLogic.GetAllCategoriesParentsAlim();
+            foreach (var item in CatParentsAlim)
+            {
+                CatEnfants1 = await categorieLogic.GetAllCategoriesEnfantsByParentId(item.Categorieid);
+
+
+            }
+            foreach (var item in CatEnfants1)
+            {
+                CatEnfants2 = await categorieLogic.GetAllCategoriesEnfantsByParentId(item.Categorieid);
+            }
         }
     }
 }
