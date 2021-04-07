c44a1bf319651b787e3c5a2ce57f163a44e7949c
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-07 15:12:15 +0200

    up

---
+++ E-Shop/E-Shop/Pages/MaBoutique/GestionBoutique/GestionBoutique.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Shop.Pages.MaBoutique.GestionBoutique
+{
+    public class GestionBoutiqueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
