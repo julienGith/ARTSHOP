8aa983e72affa8726a440c22ca20cf88512cb7b3
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-15 14:51:27 +0200

    up

--- E-Shop/E-Shop/Pages/MaBoutique/GestionBoutique/GestionBoutique.cshtml.cs
+++ E-Shop/E-Shop/Pages/MaBoutique/GestionBoutique/GestionBoutique.cshtml.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Shop.Entities;
+using E_Shop.Extensions;
+using E_Shop.Logic.BoutiqueLogic;
+using E_Shop.Logic.LocalisationLogic;
+using E_Shop.Logic.MediaLogic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +14,46 @@ namespace E_Shop.Pages.MaBoutique.GestionBoutique
 {
     public class GestionBoutiqueModel : PageModel
     {
-        public void OnGet()
+        private BoutiqueLogic boutiqueLogic = new BoutiqueLogic();
+        private MediaLogic mediaLogic = new MediaLogic();
+        private LocalisationLogic LocalisationLogic = new LocalisationLogic();
+
+        public Entities.Boutique Boutique = new Entities.Boutique();
+        public List<Medium> medias = new List<Medium>();
+        public Localisation localisation = new Localisation();
+        List<RelaisLocB> relaisLocBs = new List<RelaisLocB>();
+
+        public string imgVignette { get; set; }
+        public string imgPano { get; set; }
+        public string vid { get; set; }
+
+
+
+        public async Task<IActionResult> OnGet()
         {
+            if (HttpContext.Session.Get<int>("btqId")>0)
+            {
+                var btqId = HttpContext.Session.Get<int>("btqId");
+                Boutique = await boutiqueLogic.GetBoutiqueById(btqId);
+                medias = await mediaLogic.GetMediasBoutique(btqId);
+                foreach (var item in medias)
+                {
+                    if (item.Description=="vignette")
+                    {
+                        imgVignette = item.Lien;
+                    }
+                    if (item.Description=="pano")
+                    {
+                        imgPano = item.Lien;
+                    }
+                    if (item.Description== "boutique")
+                    {
+                        vid = item.Lien;
+                    }
+                }
+                localisation = await LocalisationLogic.GetLocalisationBoutique(btqId);
+            }
+            return Page();
         }
     }
 }
