9ca9422f5f52ba32c93d7af3538bede146a5a1a5
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-16 15:33:20 +0200

    up

--- E-Shop/E-Shop/Pages/MaBoutique/GestionPointRelais/GestionPointRelais.cshtml.cs
+++ E-Shop/E-Shop/Pages/MaBoutique/GestionPointRelais/GestionPointRelais.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Shop.Entities;
+using E_Shop.Extensions;
+using E_Shop.Logic.LocalisationLogic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,19 @@ namespace E_Shop.Pages.MaBoutique.GestionPointRelais
 {
     public class GestionPointRelaisModel : PageModel
     {
-        public void OnGet()
+        private LocalisationLogic LocalisationLogic = new LocalisationLogic();
+        public List<Localisation> pointRelais = new List<Localisation>();
+        [BindProperty]
+        public int localisationId { get; set; }
+        public int btqId { get; set; }
+        public async Task<IActionResult> OnGet()
         {
+            if (HttpContext.Session.Get<int>("btqId")>0)
+            {
+                btqId = HttpContext.Session.Get<int>("btqId");
+                pointRelais = await LocalisationLogic.GetPointRelaisByBoutiqueId(btqId);
+            }
+            return Page();
         }
     }
 }
