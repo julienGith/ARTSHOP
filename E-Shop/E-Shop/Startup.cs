e3ce9ff57274e66063248006685d4cf69734f523
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-21 16:29:10 +0200

    up

--- E-Shop/E-Shop/Startup.cs
+++ E-Shop/E-Shop/Startup.cs
@@ -28,7 +28,6 @@ namespace E_Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
             services.Configure<CookiePolicyOptions>(options =>
             {
