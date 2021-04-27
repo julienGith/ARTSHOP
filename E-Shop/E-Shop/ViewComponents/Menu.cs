c8f9d4521db7c0d030b49497ab6b5d0342aa39f4
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-27 18:58:34 +0200

    up

--- E-Shop/E-Shop/ViewComponents/Menu.cs
+++ E-Shop/E-Shop/ViewComponents/Menu.cs
@@ -18,27 +18,27 @@ namespace E_Shop.ViewComponents
         public async Task<ViewViewComponentResult> InvokeAsync()
         {
             menuCat.CatParentsAlim = await categorieLogic.GetAllCategoriesParentsAlim();
-            foreach (var item in menuCat.CatParentsAlim)
-            {
+            //foreach (var item in menuCat.CatParentsAlim)
+            //{
 
-                menuCat.CatEnfants1 = await categorieLogic.GetAllCategoriesEnfantsByParentId(item.Categorieid);
-                foreach (var cat1 in menuCat.CatEnfants1)
-                {
+            //    menuCat.CatEnfants1 = await categorieLogic.GetAllCategoriesEnfantsByParentId(item.Categorieid);
+            //    foreach (var cat1 in menuCat.CatEnfants1)
+            //    {
 
-                    menuCat.CatEnfants2 = await categorieLogic.GetAllCategoriesEnfantsByParentId(cat1.Categorieid);
+            //        menuCat.CatEnfants2 = await categorieLogic.GetAllCategoriesEnfantsByParentId(cat1.Categorieid);
 
-                    foreach (var cat2 in menuCat.CatEnfants2)
-                    {
-                        CatSubCat CatSubCat = new CatSubCat
-                        {
-                            catParent = item,
-                            catEnfants = menuCat.CatEnfants1,
-                            subCats = menuCat.CatEnfants2
-                        };
-                        menuCat.CatSubCats.Add(CatSubCat);
-                    }
-                }
-            }
+            //        foreach (var cat2 in menuCat.CatEnfants2)
+            //        {
+            //            CatSubCat CatSubCat = new CatSubCat
+            //            {
+            //                catParent = item,
+            //                catEnfants = menuCat.CatEnfants1,
+            //                subCats = menuCat.CatEnfants2
+            //            };
+            //            menuCat.CatSubCats.Add(CatSubCat);
+            //        }
+            //    }
+            //}
 
             return View("Menu",menuCat);
         }
