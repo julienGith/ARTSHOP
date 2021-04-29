4a1b2ae49c6672fa8224061c0476fe4ecfb64112
julienGith <53513531+julienGith@users.noreply.github.com>
2021-04-29 23:31:35 +0200

    up

---
+++ E-Shop/E-Shop/ViewComponents/Cart.cs
@@ -0,0 +1,27 @@
+﻿using E_Shop.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Shop.ViewComponents
+{
+    public class Cart : ViewComponent
+    {
+        
+        public async Task<ViewViewComponentResult> InvokeAsync()
+        {
+            Models.Cart cart = new Models.Cart();
+            if (HttpContext.Session.Get<Models.Cart>("Cart")!=null)
+            {
+                
+                cart = HttpContext.Session.Get<Models.Cart>("Cart");
+                return View("Cart", cart);
+            }
+            return View("Cart", cart);
+            
+        }
+    }
+}
