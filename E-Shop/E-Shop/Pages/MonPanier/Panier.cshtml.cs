745b0a9f03b6e672da1b855880941064d6aef856
julienGith <53513531+julienGith@users.noreply.github.com>
2021-05-03 19:10:34 +0200

    up

--- E-Shop/E-Shop/Pages/MonPanier/Panier.cshtml.cs
+++ E-Shop/E-Shop/Pages/MonPanier/Panier.cshtml.cs
@@ -12,7 +12,39 @@ namespace E_Shop.Pages.MonPanier
     public class PanierModel : PageModel
     {
         public Cart cart = new Cart();
-
+        [BindProperty]
+        public int prodId { get; set; }
+        [BindProperty]
+        public int quantity { get; set; }
+        public IActionResult OnPostDeleteProd()
+        {
+            cart = HttpContext.Session.Get<Models.Cart>("Cart");
+            foreach (var btq in cart.Btqs)
+            {
+                foreach (var i in btq.items)
+                {
+                    btq.items.Remove(i);
+                }
+            }
+            HttpContext.Session.Set<Models.Cart>("Cart", cart);
+            return Page();
+        }
+        public IActionResult OnPostChangeQuantity()
+        {
+            cart = HttpContext.Session.Get<Models.Cart>("Cart");
+            foreach (var btq in cart.Btqs)
+            {
+                foreach (var i in btq.items)
+                {
+                    if (i.produit.Prodid==prodId)
+                    {
+                        i.quantity = quantity;
+                    }
+                }
+            }
+            HttpContext.Session.Set<Models.Cart>("Cart", cart);
+            return Page();
+        }
         public IActionResult OnGet()
         {
             if (HttpContext.Session.Get<Models.Cart>("Cart") != null)
