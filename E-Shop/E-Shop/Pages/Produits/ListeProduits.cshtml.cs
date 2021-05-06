92832d4ecaa21f4ba36443cf28874df6097a1a8e
julienGith <53513531+julienGith@users.noreply.github.com>
2021-05-06 14:45:56 +0200

    up

--- E-Shop/E-Shop/Pages/Produits/ListeProduits.cshtml.cs
+++ E-Shop/E-Shop/Pages/Produits/ListeProduits.cshtml.cs
@@ -16,32 +16,41 @@ namespace E_Shop.Pages.Produits
     {
         private ProduitLogic produitLogic = new ProduitLogic();
         private BoutiqueLogic boutiqueLogic = new BoutiqueLogic();
-        public List<Produit> listProduits = new List<Produit>();
-        public PaginatedList<Produit> listProduitsByBtq { get; set; }
-        public List<Entities.Boutique> listBtq { get; set; }
-        public bool byProd { get; set; }
+        public List<Produit> Produits = new List<Produit>();
 
+        public PaginatedList<Produit> listProduits { get; set; }
+        public PaginatedList<Entities.Boutique> listBtq { get; set; }
+        public bool byProd { get; set; }
+        [BindProperty]
         [FromQuery(Name = "catId")]
         public int catId { get; set; }
         [BindProperty]
         public int id { get; set; }
+        [BindProperty]
+        [FromQuery(Name = "pageIndex")]
+        public int? pageIndex { get; set; }
+        [BindProperty]
+        public int? page { get; set; }
 
         public async Task<IActionResult> OnPostByProd(int catId, int? pageIndex)
         {
-            id = catId;
+            pageIndex = page;
+            catId = id;
             byProd = true;
             listProduits = await produitLogic.GetProduitsByCatId(catId, pageIndex);
             return Page();
         }
-        public async Task<IActionResult> OnGet(int? pageIndex)
+        public async Task<IActionResult> OnGet(int catId, int? pageIndex)
         {
+            page = pageIndex;
+            id = catId;
             byProd = false;
             listBtq = await boutiqueLogic.GetBoutiquesByCatId(catId, pageIndex);
             return Page();
         }
         public async Task<JsonResult> OnPostProdList(string query)
         {
-            listProduits = await produitLogic.GetListProduitByQuery(query);
+            Produits = await produitLogic.GetListProduitByQuery(query);
             var noms = listProduits.Select(p => p.PNom).ToList();
             var response = listProduits.Select(p => new { p.PNom, p.Prodid, p.Media.FirstOrDefault(m => m.Description == "min").Lien }).ToList();
             return new JsonResult(response);
