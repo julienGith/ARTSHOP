dd8b747da4153040dbf2a980f1f1f63d12e8e226
julienGith <53513531+julienGith@users.noreply.github.com>
2021-05-15 18:02:44 +0200

    up

--- E-Shop/E-Shop/Pages/MaCommande/Paiement.cshtml.cs
+++ E-Shop/E-Shop/Pages/MaCommande/Paiement.cshtml.cs
@@ -2,15 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Shop.Extensions;
+using E_Shop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 
 namespace E_Shop.Pages.MaCommande
 {
     public class PaiementModel : PageModel
     {
-        public void OnGet()
+        public PaiementModel(IConfiguration config)
         {
+            PublicKey = config["Stripe:PublicKey"].ToString();
+        }
+        public Cart cart = new Cart();
+
+        public string PublicKey { get; }
+        public IActionResult OnPostCreate()
+        {
+            cart = HttpContext.Session.Get<Models.Cart>("Cart");
+            var paymentIntents = new PaymentIntentService();
+            var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
+            {
+                Amount = (long)cart.prixTotal * 100,
+                Currency = "eur",
+
+            });
+
+            return new JsonResult(new { clientSecret = paymentIntent.ClientSecret });
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
         }
     }
 }
